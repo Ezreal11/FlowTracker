9ab35951a3bd04a21d97308d7a9d3193a26ca62d
jzyhywxz <jzyhywxz4zzw@gmail.com>
2019-07-09 10:07:27 +0800

    initialize 'interaction-activity-tracker' project by zzw on July 9 2019

---
+++ interaction-activity-tracker/src/main/java/edu/nju/ics/frontier/EntryPoint.java
@@ -0,0 +1,163 @@
+package edu.nju.ics.frontier;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import edu.nju.ics.frontier.scale.control.LikertScale;
+import edu.nju.ics.frontier.util.OkTextWriter;
+import edu.nju.ics.frontier.agent.*;
+import edu.nju.ics.frontier.persist.Persistence;
+import edu.nju.ics.frontier.util.IntelliJInfo;
+import edu.nju.ics.frontier.view.ScaleManager;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * the entry point of this plugin, this plugin is enable if at least one project is opened.
+ */
+public class EntryPoint implements ProjectComponent {
+    /**
+     * current opened project
+     */
+    private Project project;
+
+    /**
+     * action agent to manage the action listener
+     */
+    private MyActionAgent actionAgent;
+
+    /**
+     * key, mouse and window agent to manage the key, mouse and window listener
+     */
+    private MyAWTEventAgent awtEventAgent;
+
+    /**
+     * compile agent to manage the compile listener
+     */
+    private MyCompileAgent compileAgent;
+
+    /**
+     * cursor agent to manage the cursor listener
+     */
+    private MyCursorAgent cursorAgent;
+
+    /**
+     * debug agent to manage the debug listener
+     */
+    private MyDebugAgent debugAgent;
+
+    /**
+     * execute agent to manage the execution listener
+     */
+    private MyExecuteAgent executeAgent;
+
+    /**
+     * file agent to manage the file and document listener
+     */
+    private MyFileAgent fileAgent;
+
+    /**
+     * persistence to persist the captured IntelliJ events
+     */
+    private Persistence persistence;
+
+    /**
+     * scale manager to manage the subjective scale
+     */
+    private ScaleManager scaleManager;
+
+    /**
+     * logger to monitor the status of this plugin
+     */
+    public static final OkTextWriter LOGGER = new OkTextWriter();
+
+    /*
+     * create and open the logger
+     */
+    static {
+        String userHome = System.getProperty("user.home");
+        String dateStr = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String path = userHome + File.separator + "interaction_traces" + File.separator +
+                "plugin_log" + File.separator + dateStr + ".log";
+        LOGGER.open(path);
+        LOGGER.println(IntelliJInfo.getAllVersion());
+    }
+
+    /**
+     * constructor
+     * @param project created by IntelliJ IDEA
+     */
+    public EntryPoint(Project project) {
+        LOGGER.println("EntryPoint constructor");
+        this.project = project;
+    }
+
+    @Override
+    public void initComponent() {
+        LOGGER.println("initComponent");
+        // create the agent instances
+        actionAgent = new MyActionAgent();
+        awtEventAgent = new MyAWTEventAgent();
+        compileAgent = new MyCompileAgent(project);
+        cursorAgent = new MyCursorAgent();
+        debugAgent = new MyDebugAgent(project);
+        executeAgent = new MyExecuteAgent();
+        fileAgent = new MyFileAgent();
+        persistence = Persistence.getInstance();
+        scaleManager = ScaleManager.getInstance();
+    }
+
+    @Override
+    public void disposeComponent() {
+        LOGGER.println("disposeComponent");
+        // destroy the agent instances
+        actionAgent = null;
+        awtEventAgent = null;
+        compileAgent = null;
+        cursorAgent = null;
+        debugAgent = null;
+        executeAgent = null;
+        fileAgent = null;
+        persistence = null;
+        scaleManager = null;
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "Interaction Activity Tracker";
+    }
+
+    @Override
+    public void projectOpened() {
+        // start the persistence
+        persistence.execute();
+        LOGGER.println("projectOpened");
+    }
+
+    @Override
+    public void projectClosed() {
+        // dispose agents
+        Disposer.dispose(actionAgent);
+        Disposer.dispose(awtEventAgent);
+        Disposer.dispose(compileAgent);
+        Disposer.dispose(cursorAgent);
+        Disposer.dispose(debugAgent);
+        Disposer.dispose(executeAgent);
+        Disposer.dispose(fileAgent);
+        Disposer.dispose(scaleManager);
+
+        // show the subjective scale
+        String rootPath = System.getProperty("user.home") + File.separator +
+                "interaction_traces" + File.separator + "scale";
+        LikertScale scale = new LikertScale(rootPath, true);
+        scale.showLikertScaleDialog(false);
+
+        // shutdown the persistence instance
+        persistence.shutdown();
+        LOGGER.println("projectClosed");
+    }
+}
---
+++ interaction-activity-tracker/src/main/java/edu/nju/ics/frontier/listener/MyFileEditorManagerListener.java
@@ -0,0 +1,67 @@
+package edu.nju.ics.frontier.listener;
+
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.*;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.vfs.VirtualFile;
+import edu.nju.ics.frontier.persist.IntelliJEvent;
+import edu.nju.ics.frontier.persist.Persistence;
+import edu.nju.ics.frontier.util.VirtualFileUtil;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * <strong>MyFileEditorManagerListener</strong> implements <em>FileEditorManagerListener</em>
+ * to listen the status of the files in IDE.
+ */
+public class MyFileEditorManagerListener implements FileEditorManagerListener {
+    private static final String TYPE = "File";
+
+    @Override
+    public void fileOpenedSync(@NotNull FileEditorManager source, @NotNull VirtualFile file, @NotNull Pair<FileEditor[], FileEditorProvider[]> editors) {}
+
+    @Override
+    public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+        onFileChanged(System.currentTimeMillis(), "opened", file);
+    }
+
+    @Override
+    public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+        onFileChanged(System.currentTimeMillis(), "closed", file);
+    }
+
+    @Override
+    public void selectionChanged(@NotNull FileEditorManagerEvent event) {
+        long time = System.currentTimeMillis();
+        VirtualFile oldFile = (event == null) ? null : event.getOldFile();
+        VirtualFile newFile = (event == null) ? null : event.getNewFile();
+        IntelliJEvent e = new IntelliJEvent(time, TYPE, "selected");
+        e.addExtraData("oldPath", VirtualFileUtil.getPath(oldFile));
+        e.addExtraData("newPath", VirtualFileUtil.getPath(newFile));
+        Persistence.getInstance().add(e);
+    }
+
+    /**
+     * construct an <strong>IntelliJEvent</strong> for a file event
+     * and persist it in local file system.
+     * the specific data for the file IntelliJEvent consists:
+     * <ul>
+     *     <li>path: MD5-coded path of the document</li>
+     *     <li>fileLen: length of the corresponding file of the document</li>
+     *     <li>docLen: length of the document</li>
+     *     <li>line: line of code in the document</li>
+     * </ul>
+     * @param time occurrence time of this event
+     * @param when when file status changed
+     * @param file the virtual file
+     */
+    private void onFileChanged(long time, String when, VirtualFile file) {
+        Document document = VirtualFileUtil.getDocument(file);
+
+        IntelliJEvent e = new IntelliJEvent(time, TYPE, when);
+        e.addExtraData("path", VirtualFileUtil.getPath(file));
+        e.addExtraData("fileLen", VirtualFileUtil.getFileLengthString(file));
+        e.addExtraData("docLen", VirtualFileUtil.getDocumentLengthString(document));
+        e.addExtraData("line", VirtualFileUtil.getDocumentLineString(document));
+        Persistence.getInstance().add(e);
+    }
+}
